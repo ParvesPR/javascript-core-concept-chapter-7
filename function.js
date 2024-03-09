8876ca618c0644e8eb480c36949834a90adc31a4
ParvesPR <parves2585@gmail.com>
2024-03-09 13:57:13 +0800

    basic structure of function

---
+++ function.js
@@ -0,0 +1,11 @@
+// Basic structure of function
+function add() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
+function sub() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
