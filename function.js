15f360ea630f8deefa267297633ebeeda40cce1a
ParvesPR <parves2585@gmail.com>
2024-03-10 13:04:45 +0800

    invoking function

--- function.js
+++ function.js
@@ -5,7 +5,10 @@ function add() {
     console.log(a + b);
 }
 function sub() {
-    var a = 10;
-    var b = 20;
-    console.log(a + b);
+    var a = 20;
+    var b = 10;
+    console.log(a - b);
 }
+
+add();
+sub();
