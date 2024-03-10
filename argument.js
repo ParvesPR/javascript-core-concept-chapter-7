7e8181534853f008467cee980ef82c285fcf2a8d
ParvesPR <parves2585@gmail.com>
2024-03-10 13:36:41 +0800

    summation multiple array using function

--- argument.js
+++ argument.js
@@ -3,3 +3,19 @@ function add(a, b) {
     console.log(result);
 }
 console.log(10 + 20);
+
+// Summation using function
+var arr1 = [1, 3, 5]
+var arr2 = [7, 9, 11]
+var arr3 = [13, 15, 17]
+
+function sumArray(arr) {
+    var total = 0;
+    for (var x = 0; x < arr.length; x++) {
+        total += arr[x];
+    }
+    console.log(total);
+}
+sumArray(arr1)
+sumArray(arr2)
+sumArray(arr3)
